0218eeb61b4f080115615029222666623dd9a0c5
Nikhil Choudhary <116430126+Nikhil2072001@users.noreply.github.com>
2023-04-16 17:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your code here
-const msg = document.querySelector("#txt");
-const res = document.querySelector('#para');
+const msg = document.querySelector("#evaluatedText");
+const res = document.querySelector('#letterCount');
 msg.addEventListener('input',function(){
 	var s = this.value;
 	res.textContent =s.length;
