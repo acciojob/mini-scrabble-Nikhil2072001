c2cd119291fcebb6e3856ea53da9ce721472b4de
Nikhil Choudhary <116430126+Nikhil2072001@users.noreply.github.com>
2023-04-16 17:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+const msg = document.querySelector("#txt");
+const res = document.querySelector('#para');
+msg.addEventListener('input',function(){
+	var s = this.value;
+	res.textContent =s.length;
+});
